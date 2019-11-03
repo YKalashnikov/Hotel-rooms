1da36bf2d55679e766ca5f88cc3808298214263a
YKalashnikov <uk2709@gmail.com>
2019-11-03 10:16:06 -0800

    first

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { FaAlignRight } from "react-icons/fa";
+export default class Footer extends Component {
+
+  render() {
+    return (
+      <nav className="footer">
+        <div className="foot-center">
+            <li>
+              <a  href="tel:+7777777777">Call Us</a>
+            </li>
+            <li>
+              <Link>Chat</Link>
+            </li>
+            <li>
+              <Link>Company</Link>
+            </li>
+        </div>
+      </nav>
+    );
+  }
+}
